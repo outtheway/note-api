450ae29e577c888d24383bfa411e95cc3f1752ab
outtheway <132990784+outtheway@users.noreply.github.com>
2023-08-17 13:07:54 +0600

    Added @PostMapping annotation

--- src/main/java/com/nurseitov/notesapi/controller/NoteController.java
+++ src/main/java/com/nurseitov/notesapi/controller/NoteController.java
@@ -3,13 +3,17 @@ package com.nurseitov.notesapi.controller;
 import com.nurseitov.notesapi.model.Note;
 import com.nurseitov.notesapi.service.NoteService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class NoteController {
+
     private NoteService noteService;
+
+    @PostMapping("/create-note")
     public void createNote(@RequestBody Note note){
         noteService.createNote(note.getTitle(), note.getContent());
     }
